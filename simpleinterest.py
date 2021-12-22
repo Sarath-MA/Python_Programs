e48971a13ba65923b225b5e9de31d3f92896f57c
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2021-12-22 11:02:12 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,6 +1,6 @@
-P = int(input("Enter a Principal amount  "))
-R = int(input("Enter a rate of interest  "))
-T = int(input("Enter the time  "))
+P = float(input("Enter a Principal amount  "))
+R = float(input("Enter a rate of interest  "))
+T = float(input("Enter the time  "))
 
 #formula for simple interest
 si = (P * R * T/100)
