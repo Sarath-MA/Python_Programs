c4ebb4681762839427232de78da2a8efe5bf549a
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2021-12-22 07:27:44 +0530

    Create simpleinterest.py

---
+++ simpleinterest.py
@@ -0,0 +1,7 @@
+P = int(input("Enter a Principal amount  "))
+R = int(input("Enter a rate of interest  "))
+T = int(input("Enter the time  "))
+
+#formula for simple interest
+si = (P * R * T/100)
+print("The simple interest is", si)
