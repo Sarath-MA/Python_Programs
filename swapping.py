3f573b7db739ba553d8c0cedb2ab28379e7eb06b
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2021-12-25 19:03:47 +0530

    Update swapping.py

--- swapping.py
+++ swapping.py
@@ -1,9 +1,22 @@
-#Swap variables
+#Swap two variables
 
 #Using tuples
-a = 6
-b = 7
+a = input("Enter a value for a ")
+b = input("Enter a value for b ")
 
 a,b = b,a
-print(a)
-print(b)
+print("The value of a is after swapping",a)
+print("The value of b is after swapping",b)
+
+
+
+#using Temporary variable
+a = input("Enter a value for a ")
+b = input("Enter a value for b ")
+
+temp1 = a
+a = b
+b = temp1
+
+print("The value of a is after swapping",a)
+print("The value of b is after swapping",b)
