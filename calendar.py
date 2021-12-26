13a9daabf7cb5d5a866dabb779b45fa5605a2078
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2021-12-26 11:11:13 +0530

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,12 @@
+#import calendar module
+import calendar
+
+#getting user input
+yy = int(input("Enter  a year "))
+mm = int(input("Enter a month "))
+
+
+calendar = calendar.month(yy,mm)
+print(calendar)
+         
+         
