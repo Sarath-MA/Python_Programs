beca880d416c1515f4f0662fafa8860af460b12e
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2021-12-26 10:38:54 +0530

    Create celsius_to_fahrenheit.py

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+#Python program to convert celsius to fahrenheit
+celsius = float(input("Enter a degree of fahrenheit " ))
+
+#formula for converting fahrenheit to celsius
+f = celsius * (9/5) + 32
+print(f)
