27b6df842b59384cc2b3c1d56a21aab1cb58a8d0
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2022-01-02 08:20:04 +0530

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -3,3 +3,10 @@
 val = input("Enter the value ")
 val1 = ord(val)
 print(val1)
+
+
+# using for loop for string of characters
+val = input("Enter a characters ")
+for i in val:
+    val1 = ord(i)
+    print(i, "\t", val1)
