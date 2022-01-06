82acb79c2c936a8a4680e92009788c94eeed2a7a
Sarath M A <79849607+Sarath-MA@users.noreply.github.com>
2022-01-06 20:18:16 +0530

    Create sumofdigits.py

---
+++ sumofdigits.py
@@ -0,0 +1,9 @@
+#program to create sum of digits
+num = int(input("Enter a number "))
+copy = num
+sum = 0
+while num > 0:
+    r = num % 10
+    sum = sum + r
+    num //= 10
+print("The sum of digits are",sum)
